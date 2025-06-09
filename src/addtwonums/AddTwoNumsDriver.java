8f5a0598954f9dd5b559e50d0a61cd9fdd0defe2
Collier-A-B <benjaminac46@gmail.com>
2025-06-09 12:59:17 -0400

    First Testcase passed

--- src/addtwonums/AddTwoNumsDriver.java
+++ src/addtwonums/AddTwoNumsDriver.java
@@ -1,5 +1,57 @@
 package addtwonums;
 
 public class AddTwoNumsDriver {
+    public static void main(String[] args) {
+        // list representation of 342
+        int[] list1 = {2, 4, 3};
+        // list representation of 465
+        int[] list2 = {5, 6, 4};
+        // List representation of expected result (807)
+        int[] expected = {7, 0, 8};
 
+        // Test 1 (342 + 465)
+        test(list1, list2, 1, expected); 
+    }
+
+    private static ListNode makeList(int[] list){
+        // convert list 1 to linked-list
+        ListNode listRoot = null;
+
+        // current node being created
+        ListNode listNewNode = null;
+
+        // Last Node Created
+        ListNode listLastNode = null;
+        for(int i = 0; i < list.length; i++){
+            listNewNode = new ListNode(list[i]);
+            if (i == 0)
+                listRoot = listNewNode;
+            else 
+                listLastNode.next = listNewNode;
+            listLastNode = listNewNode;
+        }
+
+        return listRoot;
+    }
+
+    private static void test(int[] l1, int[] l2, int testNum, int[] expected) {
+        System.out.println("******************Test "+testNum+" ******************");
+        
+        // create new nodes 
+        ListNode l1Root = makeList(l1);
+        ListNode l2Root = makeList(l2);
+
+        // run my solution
+        ListNode result = Solution.addTwoNumbers(l1Root, l2Root);
+
+        // compare result to expected
+        for (int elem : expected){
+            System.out.println("Expected: " + elem);
+            System.out.println("Result:   " + result.val);
+            System.out.println("Digits match: " + (elem == result.val));
+            assert(elem == result.val);
+            result = result.next;
+        }
+
+    }
 }
--- src/addtwonums/Solution.java
+++ src/addtwonums/Solution.java
@@ -1,5 +1,36 @@
 package addtwonums;
 
 public class Solution {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        boolean carryover = false;
 
+        ListNode listRoot = null;
+        ListNode newNode = null;
+        ListNode lastNode = null;
+
+        while (l1 != null && l2 != null){
+            newNode = new ListNode(l1.val + l2.val);
+
+            if (carryover) {
+                newNode.val += 1;
+                carryover = false;
+            }
+            
+            if(newNode.val > 9){
+                carryover = true;
+                newNode.val = newNode.val % 10;
+            }
+
+            if (listRoot == null)
+                listRoot = newNode;
+            else
+                lastNode.next = newNode;
+            lastNode = newNode;
+
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        return listRoot;
+    }
 }

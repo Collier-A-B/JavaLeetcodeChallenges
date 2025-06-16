4e723b3a707cffb40e2fa26387c6789ffbe12bd2
Collier-A-B <benjaminac46@gmail.com>
2025-06-16 12:53:01 -0400

    Working solution, not optimized

--- src/mergeksortedlists/Solution.java
+++ src/mergeksortedlists/Solution.java
@@ -3,8 +3,37 @@ package mergeksortedlists;
 
 class Solution {
     public static ListNode mergeKLists(ListNode[] lists) {
+        // head of the returned list
         ListNode returnList = null;
-        
+
+        ListNode currentNode = null;
+        int lowestValIndx = -1;
+        while (true) { 
+            int lowestVal = Integer.MAX_VALUE;
+            
+            for (int i = 0; i < lists.length; i++){
+                if (lists[i] != null && lists[i].val < lowestVal)
+                {
+                    lowestVal = lists[i].val;
+                    lowestValIndx = i;
+                }
+            }
+            if (lowestVal == Integer.MAX_VALUE)
+                break;
+
+            ListNode newNode = new ListNode(lowestVal);
+            lists[lowestValIndx] = lists[lowestValIndx].next;
+
+            if (returnList == null) {
+                returnList = newNode;
+                currentNode = returnList;
+            }
+            else {
+                currentNode.next = newNode;
+                currentNode = newNode;
+            }
+        } 
+
         return returnList;
     }
 }

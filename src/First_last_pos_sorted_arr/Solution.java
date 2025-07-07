a0bff9201938b9bdf481bb7fbc380c39ab9c4c24
Collier-A-B <benjaminac46@gmail.com>
2025-07-07 17:14:00 -0400

    Driver method added

---
+++ src/First_last_pos_sorted_arr/Solution.java
@@ -0,0 +1,9 @@
+package First_last_pos_sorted_arr;
+
+public class Solution {
+    protected static int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
+
+        return result;
+    }
+}

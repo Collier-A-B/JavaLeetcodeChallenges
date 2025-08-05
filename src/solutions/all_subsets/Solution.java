a6c09db60c63a4d05c3910915a9cf8c2945dc86f
Collier-A-B <benjaminac46@gmail.com>
2025-08-05 13:40:13 -0400

    solved

--- src/solutions/all_subsets/Solution.java
+++ src/solutions/all_subsets/Solution.java
@@ -5,11 +5,34 @@ import java.util.List;
 
 public class Solution {
     protected static List<List<Integer>> subsets(int[] nums) {
-        class BackTracker {}
-        List<List<Integer>> result = new ArrayList<>();
+        class BackTracker {
+            private List<List<Integer>> result;
 
-        result.add(new ArrayList<>()); // Start with the empty subset
+            BackTracker() {
+                result = new ArrayList<>((int)Math.pow(2, nums.length));
+                result.add(new ArrayList<>());
+            }
 
-        return result;
+            void generateSubsets(int startIndx, List<Integer> subset) {
+                for (int i = startIndx; i < nums.length; i++) {
+                    subset.add(nums[i]);
+                    generateSubsets(i+1, subset);
+                    List<Integer> newSubset= new ArrayList<>(subset.size());
+                    newSubset.addAll(subset);
+                    result.add(newSubset);
+                    subset.remove(subset.size()-1);
+                }
+            }
+
+            List<List<Integer>> getResult() {
+                return this.result;
+            }
+        }
+
+        BackTracker bt = new BackTracker();
+
+        bt.generateSubsets(0, new ArrayList<>());
+
+        return bt.getResult();
     }
 }

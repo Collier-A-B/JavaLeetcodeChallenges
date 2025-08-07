04241d161e0750a7ed8bf476e5ae5a03fb3a60c7
Collier-A-B <benjaminac46@gmail.com>
2025-08-07 12:31:55 -0400

    init tests passed

--- src/solutions/count_and_say/Solution.java
+++ src/solutions/count_and_say/Solution.java
@@ -2,6 +2,26 @@ package solutions.count_and_say;
 
 public class Solution {
     protected static String countAndSay(int n) {
-        return "1";
+        if (n == 1) {
+            return "1";
+        }
+        String previous = countAndSay(n - 1);
+        StringBuilder result = new StringBuilder();
+
+        char currentChar = previous.charAt(0);
+        int currentCharOccurences = 1;
+        for (int i = 1;; i++) {
+            if(i == previous.length() || previous.charAt(i) != currentChar) {
+                result.append(currentCharOccurences);
+                result.append(currentChar);
+                if(i == previous.length())
+                    break;
+                currentChar = previous.charAt(i);
+                currentCharOccurences = 1;
+            } else {
+                currentCharOccurences += 1;
+            }
+        }
+        return result.toString();
     }
 }
